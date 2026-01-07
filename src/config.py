e96d1c302a9a7ccaf6f8dca118f6387312e27581
corelineportugal-lang <corelineportugal@gmail.com>
2026-01-07 23:35:20 +0000

    config

---
+++ src/config.py
@@ -0,0 +1,5 @@
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY", "")
+SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET", "uploads")
