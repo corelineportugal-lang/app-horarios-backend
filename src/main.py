e2d544cc8d3a4be9c1501217b9857cff4499c5f6
corelineportugal-lang <corelineportugal@gmail.com>
2026-01-07 23:06:51 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
 @app.get("/health")
 @app.head("/health")
 def health():
     return {"status": "ok"}
+
